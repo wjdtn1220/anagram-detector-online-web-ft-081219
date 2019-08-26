e910a25b56c973d58baa3d3f169e541c0d198a71
Jeong Park <wjdtn1220@gmail.com>
2019-08-26 19:16:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array_of_anagrams)
+    array_of_anagrams.select do |array|
+      @word.split("").sort == (array.split("").sort)
+    end
+  end
+end
